70d84118e95f9fab6f6e23053532d851671efaff
Gazi Dizdaroglu <orhan@gazidizdaroglu.com>
2023-01-27 14:06:34 +0300

    feat: ✨ add new relic integration

--- app/main.go
+++ app/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/newrelic/go-agent/v3/newrelic"
+	"github.com/nobuyo/nrfiber"
 
 	"github.com/ogd-sanbox/application/pkg/configs"
 	"github.com/ogd-sanbox/application/pkg/middleware"
@@ -14,6 +19,19 @@ func main() {
 
 	// Define a new Fiber app with config.
 	app := fiber.New(config)
+
+	// Add New Relic.
+	nrapp, err := newrelic.NewApplication(
+		newrelic.ConfigAppName("application"),
+		newrelic.ConfigLicense(os.Getenv("NEW_RELIC_LICENSE_KEY")),
+	)
+	if err != nil {
+		log.Printf("Error while connecting New Relic: %v", err)
+	}
+
+	app.Use(nrfiber.New(nrfiber.Config{
+		NewRelicApp: nrapp,
+	}))
 
 	// Middlewares.
 	middleware.FiberMiddleware(app) // Register Fiber's middleware for app.
